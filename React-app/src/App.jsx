319f47f75bd1a288976fab4294ce36ac83d965e5
Jeremy Ng <jeremy.ng.wk1990@gmail.com>
2024-08-21 08:58:43 +0800

    update main app jsx

--- React-app/src/App.jsx
+++ React-app/src/App.jsx
@@ -1,13 +1,28 @@
 import "./App.css";
-import Form from "./components/form";
 import React, { useState } from "react";
+import Form from "./components/form";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [query, setQuery] = useState({});
 
   return (
     <>
-      <Form submitQuery={setQuery} />
+      <div className="container" key="_main-container">
+        <div className="row mt-4" key="_form">
+          <Form submitQuery={setQuery} />
+        </div>
+
+        <div className="row mt-4" key="_results">
+          <div className="col-md-8">
+            <h1>Left column</h1>
+          </div>
+
+          <div className="col-md-4">
+            <h1>Right column</h1>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
